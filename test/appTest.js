5740aeede90853b6a3a93b3c9f05b418072ccb21
rodolopho <anen@rodolophos-MacBook-Pro.local>
2019-01-11 21:55:35 +0530

    test  update

--- test/appTest.js
+++ test/appTest.js
@@ -6,30 +6,5 @@ var $impleEvent=rewire("../app/app");
 
 
 describe('App', function(){
-	//before(function(){
-		
-		// let document={
-		// 	getElementById:function(id){
-		// 		if(testElementWithId.indexOf(id)==1){
-		// 			return true;
-		// 		}else{
-		// 			return false;
-		// 		}
-		// 	},
-		// 	querySelectorAll:function(selector){
-		// 		testElementQuery.map(function(e){
-		// 			if(e.hasOwnProperty(selector.replace('[').replace(']'))){
-		// 				return e.id;
-		// 			}
-					
-		// 		})
-		// 	},
 
-		// }
-		// $impleEvent.__set__("document",document);
-	//});
-	it('queryElementsHavingEventAttribute should return Html element with event attribute',function(){
-		expect($impleEvent.queryElementsHavingEventAttribute()).to.be.an('object').to.have.property('event');
-
-	});
 });
